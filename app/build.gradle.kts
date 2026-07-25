df0046517d2d69d577c5784888c22c6960012f69
Devcode940 <Devcode940@users.noreply.github.com>
2026-07-25 14:00:59 +0000

    Fix parcelize plugin resolution (apply without version); dedupe settings dependencyResolutionManagement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.parcelize)
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
+    id("org.jetbrains.kotlin.plugin.parcelize")
+    alias(libs.plugins.ksp)
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,13 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 
 rootProject.name = "EasyBrowser"
 include(":app")
